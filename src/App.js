fc7ea2c7fb05cefcf40c39ba0f92b38cc1ce7ebc
antonythomas123 <antonythomas993@gmail.com>
2024-07-27 13:46:54 +0530

    LandingPage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import LandingPage from "./views/LandingPage/LandingPage";
 
 function App() {
   return (
     <div className="app_container">
       <Navbar />
+      <LandingPage/>
     </div>
   );
 }
--- src/views/LandingPage/LandingPage.js
+++ src/views/LandingPage/LandingPage.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import styles from "./LandingPage.module.css";
+import { Typewriter } from "react-simple-typewriter";
+import { FaDownload, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 function LandingPage() {
   return (
-    <div>LandingPage</div>
-  )
+    <div className={styles.main_container}>
+      <div className={styles.name_container}>
+        <span>hi</span>
+        <span>I am</span>
+        <span>Antony Thomas</span>
+      </div>
+      <div className={styles.sub_heading}>
+        <span>
+          I am a&nbsp;
+          <span className={styles.typewriter}>
+            <Typewriter
+              words={["Frond End Developer", "Technology Associate"]}
+              loop
+              cursor
+              cursorStyle="|"
+              typeSpeed={100}
+              deleteSpeed={70}
+              delaySpeed={1000}
+            />
+          </span>
+        </span>
+      </div>
+
+      <div className={styles.download_container}>
+        <button className={`${styles.download_cv} ${styles.type}`}>
+          <span className={styles.dowload}>Download CV <FaDownload /></span>
+        </button>
+      </div>
+
+      <div className={styles.social_container}>
+        <span className={styles.social}>
+          <FaGithub size={30} />
+        </span>
+        <span className={styles.social}>
+          <FaLinkedin size={30} />
+        </span>
+        <span className={styles.social}>
+          <FaInstagram size={30} />
+        </span>
+      </div>
+    </div>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
