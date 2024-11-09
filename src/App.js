9a52579e4d5c8f71cf9afd8a50e221b2b441133f
antonythomas123 <antonythomas993@gmail.com>
2024-11-09 18:44:01 +0530

    Resume section

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import ContactMe from "./views/ContactMe/ContactMe";
 import LandingPage from "./views/LandingPage/LandingPage";
+import Resume from "./views/Resume/Resume";
 
 function App() {
   return (
     <div className="app_container">
       <Navbar />
       <LandingPage />
+      <Resume />
       <ContactMe />
     </div>
   );
