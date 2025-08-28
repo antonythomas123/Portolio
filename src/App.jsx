8ee7c699855a7f31342877a7c4309913c6e6ad71
antonythomas123 <antonythomas993@gmail.com>
2025-08-28 08:27:13 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,30 @@ import Works from "./views/Works/Works";
 
 function App() {
   return (
-    <div className="app_container">
+    <div className="overflow-x-hidden antialiased">
+      <div className="fixed inset-0 -z-10">
+        <div class="relative h-full w-full bg-black">
+          <div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div>
+          <div class="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div>
+        </div>
+      </div>
+
       <Navbar />
       <ToastProvider>
-        <div id="home">
-          <LandingPage />
+        <div className="p-12">
+          <div id="home">
+            <LandingPage />
+          </div>
+          <div id="resume">
+            <Resume />
+          </div>
+          <div id="works">
+            <Works />
+          </div>
+          <div id="contactme">
+            <ContactMe />
+          </div>
         </div>
-        <div id="resume">
-          <Resume />
-        </div>
-        <div id="works">
-          <Works />
-        </div>
-        <div id="contactme">
-          <ContactMe />
-        </div>
-
         <ToastContainer />
       </ToastProvider>
     </div>
@@ -32,3 +40,5 @@ function App() {
 }
 
 export default App;
+
+<div class="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,60 @@
 import React, { useState } from "react";
-import styles from "./Navbar.module.css";
-import { FaBars, FaXmark } from "react-icons/fa6";
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+  Transition,
+} from "@headlessui/react";
+import {
+  Bars3Icon,
+  HomeIcon,
+  XMarkIcon,
+  AdjustmentsHorizontalIcon,
+  BriefcaseIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/outline";
+
+const Nav = ({ name, onClick, icon }) => {
+  return (
+    <div>
+      <a
+        className="relative flex gap-2 items-center cursor-pointer after:absolute after:bg-[#00ff9b] after:h-[3px] after:w-0 after:left-0 after:bottom-[-4px] after:transition-[0.3s] hover:after:w-[80%] hover:after:rounded"
+        onClick={onClick}
+      >
+        <span>{icon}</span>
+        <span>{name}</span>
+      </a>
+    </div>
+  );
+};
 
 function Navbar() {
-  const [icon, setIcon] = useState(false);
+  const navigations = [
+    {
+      name: "Home",
+      current: true,
+      id: "home",
+      icon: <HomeIcon className="size-6" />,
+    },
+    {
+      name: "Resume",
+      current: false,
+      id: "resume",
+      icon: <AdjustmentsHorizontalIcon className="size-6" />,
+    },
+    {
+      name: "Work",
+      current: false,
+      id: "works",
+      icon: <BriefcaseIcon className="size-6" />,
+    },
+    {
+      name: "Contact",
+      current: false,
+      id: "contactme",
+      icon: <UserCircleIcon className="size-6" />,
+    },
+  ];
 
   const scrollToSection = (id) => {
     const section = document.getElementById(id);
@@ -13,37 +64,69 @@ function Navbar() {
   };
 
   return (
-    <div className={styles.navbar_main}>
-      <div>
-        <span className={styles.name}>Antony</span>
-        <span className={styles.color}>.</span>
-      </div>
-      <div
-        className={
-          icon ? `${styles.nav_wrapper} ${styles.active}` : styles.wrapper
-        }
-      >
-        <span className={styles.nav} onClick={() => scrollToSection("home")}>
-          Home
-        </span>
-        <span className={styles.nav} onClick={() => scrollToSection("resume")}>
-          Resume
-        </span>
-        <span className={styles.nav} onClick={() => scrollToSection("works")}>
-          Work
-        </span>
-        <span
-          className={styles.nav}
-          onClick={() => scrollToSection("contactme")}
-        >
-          Contact
-        </span>
-      </div>
+    <Disclosure
+      as="nav"
+      className="relative after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px"
+    >
+      <div className="relative flex text-[#FFFFFF] h-[70px] items-center justify-between px-10">
+        <div>
+          <span className="text-3xl tracking-widest">Antony</span>
+          <span className="text-[4rem] text-[#00ff9b]">.</span>
+        </div>
+        <div className="hidden sm:ml-6 sm:block">
+          <div className="flex gap-6 text-[14px] items-end tracking-widest">
+            {navigations?.map((item) => {
+              return (
+                <Nav
+                  key={item?.id}
+                  name={item?.name}
+                  icon={item?.icon}
+                  onClick={() => scrollToSection(item?.id)}
+                />
+              );
+            })}
+          </div>
+        </div>
 
-      <div className={styles.hamburger} onClick={() => setIcon(!icon)}>
-        {icon ? <FaXmark size={25} /> : <FaBars size={25} />}
+        <div className="absolute inset-y-0 right-5 flex items-center sm:hidden">
+          <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-[#00ff9b]">
+            <Bars3Icon
+              aria-hidden="true"
+              className="block size-6 group-data-open:hidden"
+            />
+          </DisclosureButton>
+        </div>
       </div>
-    </div>
+      <Transition
+        enter="transition ease-out duration-300"
+        enterFrom="opacity-0 translate-x-full"
+        enterTo="opacity-100 translate-x-0"
+        leave="transition ease-in duration-200"
+        leaveFrom="opacity-100 translate-x-0"
+        leaveTo="opacity-0 translate-x-full"
+      >
+        <DisclosurePanel className="sm:hidden fixed inset-y-0 right-0 w-2/3 max-w-3/4 bg-[#020617] flex flex-col items-center justify-center gap-6 text-white z-50">
+          <DisclosureButton className="absolute top-5 right-5 rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-[#00ff9b]">
+            <XMarkIcon className="size-8" />
+          </DisclosureButton>
+          <div className="flex flex-col text-center gap-6">
+            {navigations?.map((item) => {
+              return (
+                <DisclosureButton
+                  key={item?.id}
+                  as="a"
+                  className={"flex gap-2 items-center"}
+                  onClick={() => scrollToSection(item?.id)}
+                >
+                  {item?.icon}
+                  <span>{item?.name}</span>
+                </DisclosureButton>
+              );
+            })}
+          </div>
+        </DisclosurePanel>
+      </Transition>
+    </Disclosure>
   );
 }
 
