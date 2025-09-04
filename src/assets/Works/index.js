38933fe4f0ab9419f2e6547cb67e443a2f3fc03c
antonythomas123 <antonythomas993@gmail.com>
2025-09-04 09:49:53 +0530

    name corrected

--- src/assets/Works/index.js
+++ src/assets/Works/index.js
@@ -1,3 +1,3 @@
 export { default as LaunchLane } from "./Launchlane.png";
 export { default as Portfolio } from "./Portfolio.png";
-export { default as Ecom } from "./Ecom.png";
+export { default as Ecom } from "./ECom.png";
