26cbbf073056af1044a21293821136778cdfa3aa
antonythomas123 <antonythomas993@gmail.com>
2025-09-04 09:45:41 +0530

    New work added

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,3 @@
 export { default as ResumeAntony } from "./Antony_Thomas.pdf";
-export { LaunchLane, Portfolio } from "./Works";
+export { LaunchLane, Portfolio, Ecom } from "./Works";
 export { default as HomeImg } from "./HomeImg.jpg";
