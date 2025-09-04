e5b4ebd83114dd6a89d2e4d88ce6af628bb5668a
antonythomas123 <antonythomas993@gmail.com>
2025-09-04 11:13:10 +0530

    Tech added in works

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,40 +2,49 @@ import React from "react";
 import styles from "./Card.module.css";
 import { FaGithub, FaLink } from "react-icons/fa6";
 
-function Card({ image, hostedLink, github, title }) {
+function Card({ image, hostedLink, github, title, tech }) {
   return (
     <div className={styles.card_container}>
       <div className={styles.image_container}>
         <img src={image} alt={title || ""} height={"200px"} width={"300px"} />
       </div>
 
-      <h3 className={styles.heading}>{title}</h3>
+      <div className="p-4 flex flex-col gap-4">
+        <h2 className="font-semibold text-[14px] tracking-widest">{title}</h2>
 
-      <div
-        className={styles.bottom_container}
-        style={{ justifyContent: hostedLink ? "space-between" : "flex-end" }}
-      >
-        {hostedLink && (
-          <a
-            href={hostedLink}
-            target="_blank"
-            rel="noreferrer"
-            className={styles.social}
-          >
-            <FaLink size={18} />
-          </a>
-        )}
+        <div className="flex flex-wrap gap-2">
+          {tech.map((t, index) => {
+            return <div className="bg-[#64ffda1a] rounded-full text-[#00ff9b] px-4 py-1 text-[12px]">{t}</div>;
+          })}
+        </div>
 
-        {github && (
-          <a
-            href={github}
-            target="_blank"
-            rel="noreferrer"
-            className={styles.social}
-          >
-            <FaGithub size={18} />
-          </a>
-        )}
+        <div
+          className={`flex ${
+            hostedLink ? "justify-between" : "justify-end"
+          }  items-center`}
+        >
+          {hostedLink && (
+            <a
+              href={hostedLink}
+              target="_blank"
+              rel="noreferrer"
+              className={styles.social}
+            >
+              <FaLink size={18} />
+            </a>
+          )}
+
+          {github && (
+            <a
+              href={github}
+              target="_blank"
+              rel="noreferrer"
+              className={styles.social}
+            >
+              <FaGithub size={18} />
+            </a>
+          )}
+        </div>
       </div>
     </div>
   );
